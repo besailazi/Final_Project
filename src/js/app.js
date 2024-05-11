d8cbda288306b2e2d07792ef5ff9672db3e37f07
besailazi <126988974+besailazi@users.noreply.github.com>
2024-05-11 23:50:21 +0200

    Add UI

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import {firebaseConfig} from "./firebaseConfig"
+/*import {firebaseConfig} from "./firebaseConfig"
 
 import { initializeApp } from "firebase/app"
 
@@ -6,4 +6,52 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 
 initializeApp(firebaseConfig)
 
-const authService = getAuth();
+const authService = getAuth();*/
+
+document.addEventListener('DOMContentLoaded', function() {
+	const icons = document.querySelectorAll('.icons a');
+	const popupOverlay = document.querySelector('#popup');
+	const errorMessage = document.querySelector('.error-message');
+ 
+	function openPopup() {
+	  popupOverlay.style.display = 'flex';
+	}
+ 
+	function closePopup() {
+	  popupOverlay.style.display = 'none';
+	}
+ 
+	icons.forEach(icon => {
+	  icon.addEventListener('click', function(event) {
+		 event.preventDefault();
+		 openPopup();
+	  });
+	});
+ 
+	const closeBtn = document.querySelector('.close-btn');
+	closeBtn.addEventListener('click', closePopup);
+ 
+	// Function to show error message
+	function showError() {
+	  errorMessage.style.display = 'block';
+	}
+ 
+	// Function to hide error message
+	function hideError() {
+	  errorMessage.style.display = 'none';
+	}
+ 
+	
+	const signInButton = document.querySelector('.sign-in-button');
+	signInButton.addEventListener('click', function(event) {
+	  
+	  const signInSuccess = false; 
+ 
+	  if (!signInSuccess) {
+		 showError();
+	  } else {
+		 hideError();
+	  }
+	});
+ });
+ 
